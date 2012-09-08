bfcaf12a306bc4c944ff49438cda94d82cb9878c
Daniel Kinjerski <danielkinjerski@aol.com>
2012-09-08 18:09:55 -0400

    re added game menus

--- GameJam/Assets/Scripts/GameManager.cs
+++ GameJam/Assets/Scripts/GameManager.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
 using System.Collections;
-
+public enum GameState
+{
+    OpeningWindow = 1,
+    PlayGame = 2,
+    GameOver = 3,
+    Pause = 4
+}
+public  enum CurrentPlayMode
+{
+    Black = 1,
+    White = 2
+}
 public class GameManager : MonoBehaviour {
     public GameObject OpeningWindow, GameOverWindow;
+    public static GameState gameState;
+    public static int CurrentPlayMode;
 	// Use this for initialization
 	void Awake () {
         OpeningWindow.SetActiveRecursively(true);
