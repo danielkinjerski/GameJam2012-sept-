e7eaed0939a1a8ff66fc103555a8b840f83450d9
Daniel Kinjerski <danielkinjerski@aol.com>
2012-09-08 12:07:08 -0400

    Organized folder and added opening and game over menus

--- GameJam/Assets/Scripts/SpriteAnimation.cs
+++ GameJam/Assets/Scripts/SpriteAnimation.cs
@@ -16,7 +16,6 @@ class SpriteAnimation : MonoBehaviour
     public int columns = 2;
     public int rows = 2;
     public float framesPerSecond = 10f;
-	// THIS IS AN IRRELEVANT COMMENT
 
 
     //the current frame to display
